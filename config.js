68953a562920dafce6ffa6146125265476e21db7
John Park <e2npark@gmail.com>
2019-07-07 16:49:56 +0900

    add config.js for a sample

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: 'heartbeat', 		// your database password
+		port: 	  3306, 		// default MySQL port
+		db: 	  'my_db' 		// your database name
+	},
+	server: {
+		host: 'localhost',
+		port: '8080'
+	}
+}
+
+module.exports = config
